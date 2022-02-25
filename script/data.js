11c7bf45eaeb96bdf15fa1357304e9dd1ecfcdbd
Yash Tomar <99667382+yashtomar15@users.noreply.github.com>
2022-02-24 22:37:05 -1200

    product data

--- script/data.js
+++ script/data.js
@@ -27,3 +27,35 @@ var prodarr=[{
     ]
 
     localStorage.setItem("slideimage",JSON.stringify(slidearr));
+
+    var prodslide=[{
+    img_url:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/5_1.webp?v=1644481721",
+    name:"CICA CALMING NIGHT GEL WITH NIACINAMIDE",
+    price:"Rs: 595.00"
+},{
+    img_url:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/4_4.webp?v=1644493527",
+    name:"Vitamin C Serum Glow Revealing Concentrate",
+    price:"Rs: 926.00"
+},{
+    img_url:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1-4_876f386f-dd64-472c-8ace-4b616db63105.jpg?v=1644902669",
+    name:"Night Reset Retinol + Ceramide Treatment Cream",
+    price:"Rs: 897.00"
+},{
+    img_url:"https://cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1_adea079b-ee56-4632-8012-e9fe744cf4bc.jpg?v=1644914942",
+    name:"HYALURONIC + CERAMIDE HYDRATING FACE SERUM",
+    price: "RS 975.00"
+},{
+    img_url:"//cdn.shopify.com/s/files/1/0361/8553/8692/products/1_12.webp?v=1644862382",
+    name:"LIP SLEEPING MASK WITH VITAMIN C",
+    price:"Rs 612.00"
+},
+{
+    img_url:"//cdn.shopify.com/s/files/1/0361/8553/8692/products/Artboard1-2_52198176-cba4-4b83-b4cf-26a5455dd4fb.jpg?v=1644914660",
+    name:"Vitamin C+E Super Bright Moisturizer",
+    price:"Rs 595.00"
+}
+    ]
+
+    prodslide.map(function storeprod(elm){
+        localStorage.setItem("prodslide",JSON.stringify(prodslide))
+    })
