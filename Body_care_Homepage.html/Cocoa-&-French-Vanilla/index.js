b760b398b7575afb07ca8c5b388c3aa5b2482401
amaryamunavishwakarma <vishwakarmaamar805@gmail.com>
2022-02-27 10:00:07 +0530

    fix css isue

--- Body_care_Homepage.html/Cocoa-&-French-Vanilla/index.js
+++ Body_care_Homepage.html/Cocoa-&-French-Vanilla/index.js
@@ -44,3 +44,35 @@ function like() {
     }
    
 }
+
+var regdUser = JSON.parse(localStorage.getItem("signUpDetails")) ;
+console.log(regdUser);
+document.querySelector("#form").addEventListener("submit",formSubmit);
+ 
+var form = document.querySelector("#form");
+function  formSubmit(event)
+{
+    event.preventDefault();
+    var email = form.email.value;
+    var pass = form.pass.value ;
+    console.log(email,pass);
+    var flag = false;
+    for(var i = 0 ; i <regdUser.length ; i++)
+    {
+        if(regdUser[i].email == email && regdUser[i].password == pass)
+        {
+            flag = true;
+            break;
+        
+        }
+    }
+    if(flag == true)
+    {
+        alert("login successfull");
+        window.location.href = "index.html"
+    }
+    else{
+        alert("wrong");
+    }
+
+}
